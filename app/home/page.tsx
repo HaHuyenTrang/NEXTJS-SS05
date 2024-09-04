9077acc7e817eb4659ffc9eeef1a64e66619ee52
HaHuyenTrang <huyentrang15625@gmail.com>
2024-09-04 14:52:53 +0700

    up

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import { useRouter } from 'next/navigation'
-export default function page() {
+export default function Page() {
     const router=useRouter();
     const loginUser=()=>{
         router.push("/home/auth/login");
